f3d25dbc81ca5bccfdaa0cf8a47dbebeef0cf284
Nailson <nailson_ferreira@hotmail.com>
2019-03-18 10:55:08 -0300

    beta 0.0.1

--- CodeFirstEF/App_Start/RouteConfig.cs
+++ CodeFirstEF/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace CodeFirstEF
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "FaculdadesController", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
